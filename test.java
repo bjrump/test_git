a97a69517b1aca0152de707ad4c27d6d900f02e7
bjrump <bjrump@gmail.com>
2020-08-11 08:22:05 +0200

    test

--- test.java
+++ test.java
@@ -1,5 +1,35 @@
-class test{  
-    public static void main(String args[]){  
-     System.out.println("Hello Java");  
-    }  
-}  
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class test extends JFrame {
+    
+    private JButton button;
+    private int i = 0;
+    
+    public test(){
+        button = new JButton("click mich!");
+        button.addActionListener(new ButtonLauscher());
+        
+        this.getContentPane().add(button);
+    }
+    
+    class ButtonLauscher implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            if(e.getSource() == button){
+                i++;
+                System.out.println("Button geklickt! " + i);
+            }
+        }
+    }
+    
+    public static void main(String[] args){
+        test bec = new test();
+        bec.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        bec.setSize(200, 200);
+        bec.setVisible(true);
+    }
+}
